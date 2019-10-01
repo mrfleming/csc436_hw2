2897c841ba8cfb47c698afdc5de5734ad4cc7ad4
Eric <ericfleming8@gmail.com>
2019-10-01 17:00:36 -0500

    finished part 1

--- hw2/src/app/my-button/my-button.component.ts
+++ hw2/src/app/my-button/my-button.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MyService} from '../my-service.service';
 @Component({
   selector: 'app-my-button',
   templateUrl: './my-button.component.html',
-  styleUrls: ['./my-button.component.css']
+  styleUrls: ['./my-button.component.css'],
+  providers: [MyService]
 })
 export class MyButtonComponent implements OnInit {
 
-  constructor() { }
+  liked: boolean;
+
+  constructor(private service: MyService) {}
+
+  toggleLike() {
+    // this.liked = !this.liked;
+    this.liked = this.service.likeImage(this.liked);
+    console.log(`The state is now ${this.liked}`);
+  }
 
   ngOnInit() {
+    this.liked = false;
   }
 
 }
