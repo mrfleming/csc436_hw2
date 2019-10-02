bc7564c3c66bef5c74104041340f94287380d9c8
Eric <ericfleming8@gmail.com>
2019-10-01 23:14:44 -0500

    working on messages and user

--- hw2/src/app/app.module.ts
+++ hw2/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyImageComponent } from './my-image/my-image.component';
 import { MyButtonComponent } from './my-button/my-button.component';
+import { MessageComponent} from './models/message.component';
+import { MessagesComponent } from './models/messages.component';
+import { UserComponent } from './models/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyImageComponent,
-    MyButtonComponent
+    MyButtonComponent,
+    MessageComponent,
+    MessagesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
