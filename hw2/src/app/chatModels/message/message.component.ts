8d274f3802b62bdd3393e4315f23c967519854db
Eric <ericfleming8@gmail.com>
2019-10-02 12:41:17 -0500

    render user to the screen

--- hw2/src/app/chatModels/message/message.component.ts
+++ hw2/src/app/chatModels/message/message.component.ts
@@ -17,7 +17,7 @@ export class MessageComponent implements OnInit {
 
   setMessage(txt: string) {
     const today = new Date();
-    const day = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const day = today.toDateString();
     const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     this.timeStamp = `(${day}) --> ${time}`;
     this.text = txt;
--- hw2/src/app/chatModels/user/user.component.ts
+++ hw2/src/app/chatModels/user/user.component.ts
@@ -10,18 +10,17 @@ import { MessageComponent } from '../message/message.component';
 export class UserComponent implements OnInit {
 
   name: string;
-  history: MessagesComponent;
 
   constructor() { }
 
   ngOnInit() {
-    /*
     const r = Math.round(1000000 * Math.random());
     this.name = `user${r}`;
-    const m = new MessageComponent('Signed on...');
-    this.history = new MessagesComponent();
-    this.history.append(m);
-    */
+    /**
+     * const m = new MessageComponent('Signed on...');
+     * this.history = new MessagesComponent();
+     * this.history.append(m);
+     */
   }
 
 }
